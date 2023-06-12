30e7dadafa22f1328ae01caa460ae5ae7d143937
Allam9573 <aallamdaniel@gmail.com>
2023-06-12 02:59:45 -0600

    prototipo cubo casi listo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public SpriteRenderer sp;
     [Header("Capas")]
     public LayerMask layerMask;
     [Header("Estadisticas")]
@@ -17,10 +18,11 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Color customColor = new Color(0.4f, 0.9f, 0.7f, 1.0f);
     }
     void Awake(){
         rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -42,13 +44,13 @@ public class PlayerController : MonoBehaviour
     void Walk(){
         if(puedeMover){
             rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
-            if(direction != Vector2.zero){
-                if(direction.x < 0 && this.transform.localScale.x > 0){
-                    this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
-                }else if(direction.x > 0 && this.transform.localScale.x < 0){
-                    this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
-                }
-            }
+            // if(direction != Vector2.zero){
+            //     if(direction.x < 0 && this.transform.localScale.x > 0){
+            //         this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+            //     }else if(direction.x > 0 && this.transform.localScale.x < 0){
+            //         this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+            //     }
+            // }
         }
     }
     private void Jump(){
@@ -60,6 +62,11 @@ public class PlayerController : MonoBehaviour
             rb.velocity += Vector2.up * Physics2D.gravity.y * (2.5f - 1) * Time.deltaTime;
         }else if(rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space)){
             rb.velocity += Vector2.up * Physics2D.gravity.y * (2.0f - 1) * Time.deltaTime;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "c1"){
+            // other.gameObject.sp.color = Color.red;
         }
     }
 }
